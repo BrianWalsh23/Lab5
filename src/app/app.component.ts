565ce6dab85edf0ed9c8c21a371d22eb0ca74e09
unknown <g00424410@atu.ie>
2024-03-06 11:40:45 +0000

    lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StudentService } from './Services/student.service';
+import { CommonModule } from '@angular/common';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'student-app';
+export class AppComponent implements OnInit{
+  students : any = [];
+  weather : any = [];
+  temp : any = "";
+
+  constructor(private studentService:StudentService, private weatherService:WeatherService){}
+  
+
+  ngOnInit(): void {
+    this.studentService.GetStudentData().subscribe(
+        (data)=>{
+          this.students = data.students;
+        }
+    );
+
+    this.weatherService.GetWeather().subscribe(
+      (data)=>{
+        this.weather = data.weather;
+        this.temp = data.main;
+      }
+    );
+  }
+
+    
+
 }
